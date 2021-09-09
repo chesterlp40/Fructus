e740a62b05df53d5700c0fd17d5f753b62bd613f
Ezequiel Rasgido <ezequiel.rasgido@mercadolibre.com>
2021-09-09 01:07:03 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ezequiel Rasgido on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
